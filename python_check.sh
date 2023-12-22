dcae30060411738b548ce414bac30961b634addd
WillemGibson <willemjackgibson@gmail.com>
2023-12-22 14:03:06 +1100

    Adding run.sh

--- python_check.sh
+++ python_check.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-if ! [[ -x "$(command -v python)" ]]
+if [[ -x "$(command -v python3)" ]] 
 then
-  echo 'Error: 
-    This program runs on Python, but it looks like Python is not installed.
-    To install Python, check out https://installpython3.com/' >&2
-  exit 1
+    pyv="$(python3 -V 2>&1)"
+    if [[ $pyv != "Python 3"* ]] 
+    then
+        echo "Python 3.9.x or higher is recommended for running this program." >&2
+    else
+        echo "You have Python 3 installed. To continue, please run 'run.sh." >&2
+    fi 
+else
+    echo "This program requires Python 3 to be installed.
+    Please install Python at https://www.python.org/downloads/" >&2
 fi
--- run.sh
+++ run.sh
@@ -3,7 +3,4 @@ cd "$(dirname "$0")"
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
-echo "#!/bin/bash"
-echo "cd '$(dirname "$0")'"
-echo "source .venv/bin/activate" 
-echo "python main.py"
+python3 main.py
